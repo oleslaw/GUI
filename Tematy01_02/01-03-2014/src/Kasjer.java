17d5ad9c70ad6e304f4fea99a6a96d2991f16c71
Olek Winecki <oleslaw@gmail.com>
2014-04-03 22:01:04 +0200

    dodanie zadania 11

--- Tematy01_02/01-03-2014/src/Kasjer.java
+++ Tematy01_02/01-03-2014/src/Kasjer.java
@@ -1,24 +1,72 @@
 
 
-public class Kasjer {
+public class Kasjer extends Thread{
 	
 	public static final int WPLATA = 1;
 	public static final int WYPLATA = 2;
 	public static final int PRZELEW = 3;
+	private int wybranaOperacja;
+	private int kwota;
+	private int ilePowtorzen;
+	private Rachunek wybranyRach;
+	private Rachunek odbiorcaPrzelewu;
 	
 
-	public Kasjer(){
-		
+	public Kasjer(Rachunek rach, int kwota, int operacja, int ilePowtorzen){
+		this.wybranaOperacja = operacja;
+		this.kwota = kwota;
+		this.ilePowtorzen = ilePowtorzen;
+		this.wybranyRach = rach;
 	}
 	
-	public static void WPLATA(){
-		System.out.println("Wplata adsasda");
+	public Kasjer(Rachunek rach, Rachunek odbiorcaPrzelewu, int kwota, int operacja, int ilePowtorzen){
+		this(rach, kwota, operacja, ilePowtorzen);
+		this.odbiorcaPrzelewu = odbiorcaPrzelewu;
 	}
 	
-	public static void WYPLATA(){
-		System.out.println("wyplata jajaja");
+	synchronized public static void WPLATA(Rachunek rach, int ilePowtorzen, int kwota){
+		for(int i = 0; i < ilePowtorzen; i++){
+			rach.wplata(kwota);
+		}
 	}
 	
+	synchronized public static void WYPLATA(Rachunek rach, int ilePowtorzen, int kwota){
+		int ileUdanych = 0;
+		for(int i = 0; i < ilePowtorzen; i++){
+			if(rach.stanKonta >= kwota){
+				rach.wyplata(kwota);
+				ileUdanych++;
+			}else{
+				System.out.println("Uda³o siê wykonaæ tylko " + ileUdanych + " operacji: wyp³ata");
+				break;
+			}
+		}
+	}
+	
+	synchronized public static void PRZELEW(Rachunek nadawcaPrzelewu, Rachunek odbiorcaPrzelewu, int ilePowtorzen, int kwota){
+		int ileUdanych = 0;
+		for(int i = 0; i < ilePowtorzen; i++){
+			if(nadawcaPrzelewu.stanKonta >= kwota){
+				nadawcaPrzelewu.przelew(odbiorcaPrzelewu, kwota);
+				ileUdanych++;
+			}else{
+				System.out.println("Uda³o siê wykonaæ tylko " + ileUdanych + " operacji: wyp³ata");
+				break;
+			}
+		}
+	}
+	
+	public void run(){
+		if(wybranaOperacja == WPLATA){
+			WPLATA(wybranyRach, ilePowtorzen, kwota);
+		}else if(wybranaOperacja == WYPLATA){
+			WYPLATA(wybranyRach, ilePowtorzen, kwota);
+		}else if(wybranaOperacja == PRZELEW){
+			PRZELEW(wybranyRach, odbiorcaPrzelewu, ilePowtorzen, kwota);
+		}else{
+			System.out.println("B³êdna operacja!");
+		}
+	}
 	
 	
 }
--- Tematy01_02/01-03-2014/src/Z11Test.java
+++ Tematy01_02/01-03-2014/src/Z11Test.java
@@ -11,7 +11,44 @@ public class Z11Test {
             new Rachunek(new Osoba("firma 5"), 100)
         };
         
-        new Kasjer(ror[0], 10, Kasjer.WPLATA, 10000);
+        Thread kas[] = new Kasjer[] {
+                new Kasjer(ror[0], 10, Kasjer.WPLATA, 10000),   // kasjer 1: 10000-krotna wp³ata kwoty 10 na konto firmy 1 
+                new Kasjer(ror[0], 5, Kasjer.WPLATA, 10000),     // kasjer 2: 10000-krotna wp³ata kwoty 5 na konto firmy 1
+                
+                new Kasjer(ror[1], 50, Kasjer.WYPLATA, 10000),  // kasjer 3: 10000-krotna wyp³ata kwoty 50 z konta firmy 2
+                new Kasjer(ror[1], 50, Kasjer.WYPLATA, 10000),  // kasjer 4: 10000-krotna wyp³ata kwoty 50 z konta firmy 2
+                
+                new Kasjer(ror[2], ror[3], 5, Kasjer.PRZELEW, 10000), // kasjer 5: 10000-krotny przelew kwoty 5 z konta firmy 3 na firmy 4
+                new Kasjer(ror[3], ror[2], 5, Kasjer.PRZELEW, 10000),  // kasjer 6: 10000-krotny przelew kwoty 5 z konta firmy 4 na firmy 3
+                
+                new Kasjer(ror[3], 10, 123, 10000),   // kasjer 7: 10000-krotna operacja 123 (kwoty 10) dotycz¹ca firmy 4 - b³êdna operacja 123! 
+                
+                new Kasjer(ror[4], 10, Kasjer.WYPLATA, 200) // kasjer 8: 200-krotna wyp³ata kwoty 10 z konta firmy 5 - uda siê wykonaæ tylko 10 (z 200) operacji!
+            };
+        
+        for (Thread k : kas)
+            k.start();
+        
+            // czekanie na zakoñczenie wszystkich w¹tków
+        try {
+            for (Thread k : kas)
+                k.join();
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        
+        // poprawne wyniki:      
+        // firma 5, stan: 0.0 - niedozwolona operacja: wyp³ata!
+        // Uda³o siê wykonaæ tylko 10 operacji: wyp³ata
+        // B³êdna operacja!
+        // firma 1, stan: 150000.0
+        // firma 2, stan: 0.0
+        // firma 3, stan: 120000.0
+        // firma 4, stan: 140000.0
+        // firma 5, stan: 0.0
+        for (Rachunek r : ror) {
+            System.out.println(r);
+        }
 
 	}
 
